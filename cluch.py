ca17ed676bebb446f0233a2016eeae5fcf0cd9e5
rohit jogson <rohit@darkbears.com>
2018-01-25 18:06:26 +0530

    all

--- cluch.py
+++ cluch.py
@@ -1,9 +1,10 @@
 #! /usr/bin/python3
-
+import threading
 import requests
 from bs4 import BeautifulSoup as Bs
 import re
 import pymysql as sql
+import time
 
 conn=sql.Connect('localhost','root','')
 cur=conn.cursor()
@@ -13,6 +14,192 @@ except Exception as e:
     print(e)
 
 cur.execute('use clutch_co')
+
+
+class query_exe(threading.Thread):
+    def __init__(self,company_id,file_u,company_name,company_contact,company_website,company_location_city,company_location_region,company_employee,company_rating,company_review_count):
+        super(query_exe,self).__init__()
+        self.company_id=company_id
+        self.file_u=file_u
+        self.company_name=company_name
+        self.company_contact=company_contact
+        self.company_website=company_website
+        self.company_location_city=company_location_city
+        self.company_location_region=company_location_region
+        self.company_employee=company_employee
+        self.company_rating=company_rating
+        self.company_review_count=company_review_count
+    def run(self):
+        query='insert into clutch_db values("'+self.company_id+'","'+self.file_u+'","'+self.company_name+'","'+self.company_contact+'","'+self.company_website+'","'+self.company_location_city+'","'+self.company_location_region+'","'+self.company_employee+'","'+self.company_rating+'","'+self.company_review_count+'")'
+            
+
+        try:    
+            cur.execute(query)
+            
+            
+            print('inserted company'+ company_name)
+        except Exception as a:
+            
+            print('query not inserted cause :',a)
+        try:
+            conn.commit()
+        except Exception as r:
+            print('fsdfs',r)   
+
+
+
+class fetchdata(threading.Thread):
+    def __init__(self,num_of_row, new_url,file_u,i):
+        super(fetchdata, self).__init__()
+        self.i=i
+        self.new_url=new_url
+        self.file_u=file_u
+        self.num_of_row=num_of_row
+    def run(self): 
+    
+        new_url1=self.new_url+'?page='+str(self.i)
+        
+        print('connecting...............for page '+str(self.i)+ 'of' +str(self.num_of_row))
+        
+        lunk=self.file_u+'_page'+str(self.i)+'.text'
+        try:
+            fp=open('cat_page/'+lunk,'r')
+            
+            page_data=fp.read()
+            fp.close()
+            if(self.num_of_row==self.i+1):
+                fd=open('compelate.text','a')
+                fd.write(new_url)
+                fd.close()
+            
+        except Exception as e:
+            while True:
+                print(new_url1)
+                try:    
+                    page_data=requests.get(new_url1,timeout=10)
+                    if(page_data.status_code==200):
+                        
+                        fp=open('cat_page/'+lunk,'w+')
+                        fp.write(page_data.text)
+                        page_data=page_data.text
+                        fp.close()
+                        
+                except Exception as e1:
+                    print('trying again after some time due to ',e1)        
+                    time.sleep(2)
+
+            
+        
+        page_1_soup=Bs(page_data,'html.parser')
+        
+
+        page_all_lis=page_1_soup.find_all('li',class_='provider-row')
+
+        
+        #all seperate 
+        count=0
+        for lis in page_all_lis:
+            li_data=lis.find('div',class_='row')
+            try:
+                company_location_country=li_data.find('span',class_='country-name').text
+            except Exception as e:
+                print('company_region not fount ')
+                company_location_country=''
+            print('company_cccc ',company_location_country)
+            try:
+                company_id=li_data['data-clutch-nid']
+            except Exception as e:
+                print('company_id not fount ')
+                company_id=''
+            query1='update clutch_db set company_location_country="'+company_location_country+'" where company_id="'+company_id+'" '
+            try:
+                cur.execute(query1)
+                conn.commit()
+            except Exception as p:
+                print('update ',p)
+            # try:
+            #     company_location_region=li_data.find('span',class_='region').text
+            # except Exception as e:
+            #     print('company_region not fount ')
+            #     company_location_region=''
+            # #print('region= '+company_location_region)
+            
+
+
+            # try:
+            #     company_id=li_data['data-clutch-nid']
+            # except Exception as e:
+            #     print('company_id not fount ')
+            #     company_id=''
+            # #print ('company_id='+company_id)
+            # try:
+            #     company_website=li_data.find('li',class_='website-link website-link-a').a['href']
+            # except Exception as e:
+            #     print('company_website not fount for  '+company_name)
+            #     company_website=''
+
+            # try:
+            #     company_name=li_data.find('h3',class_='company-name').text.strip()
+            # except Exception as e:
+            #     ind=re.match(r'http://(www)?(.*)\.',company_website).group()
+            #     company_name=ind.replace(r'http://','').replace('www.','').replace('.','')
+            # if(company_name==''):
+            #     ind=re.match(r'http://(www)?(.*)\.',company_website).group()
+            #     company_name=ind.replace(r'http://','').replace('www.','').replace('.','')
+                
+            
+            # #print('company_name = '+company_name)
+
+            # try:
+            #     company_rating=li_data.find('span',class_='rating').text
+                
+            # except Exception as e:
+            #     print('company_rating not fount ')
+            #     company_rating=''
+            
+            # #print('company_rating='+company_rating)
+            
+            # try:
+            #     company_review_count=li_data.find('span',class_='reviews-count').a.text.split(' ')[0]
+            
+            # except Exception as e:
+            #     print('company_review not fount ')
+            #     company_review_count=''
+            
+            # #print('company_review='+company_review_count)
+            
+            
+            # try:
+            #     company_employee=li_data.find('span',class_='employees').text
+            # except Exception as e:
+            #     print('company_emp not fount ')
+            #     company_employee=''
+            
+            # #print('num_employee= '+company_employee)
+
+            
+            
+            # try:
+            #     company_location_city=li_data.find('span',class_='locality').text[:-1]
+            # except Exception as e:
+            #     print('company_city not fount ')
+            #     company_location_city=''
+            
+            # #print('company_location_city= '+company_location_city)
+            # #print('company_website= '+company_website)
+            # try:
+            #     company_contact=li_data.find('div',class_='item __color6a').text.strip()
+            # except Exception as e:
+            #     print('company_contact not fount ')
+            #     company_contact=''
+            
+            # #print('company_contact='+company_contact)
+            # tr=query_exe(company_id,file_u,company_name,company_contact,company_website,company_location_city,company_location_region,company_employee,company_rating,company_review_count).start()
+            # tr.join()
+            
+           
+        print('Thread Completed')
+                
 
 #url='https://clutch.co'
 
@@ -27,8 +214,10 @@ cur.execute('use clutch_co')
 categories_Url='https://clutch.co/directories'
 
 print('connecting...........for initialising')
-categories_html=requests.get(categories_Url)
-
+try:
+    categories_html=requests.get(categories_Url)
+except Exception as p:
+    print(p)
 categories_Soup=Bs(categories_html.text,'html.parser')
 #print(categories_Soup.prettify())
 categories_h2=categories_Soup.find_all('h2')
@@ -85,11 +274,27 @@ for head in header_item[:]:
         url='https://clutch.co'
 
         new_url=url+cat_link[links]
-
+        # try:
+        #     fd=open('compelate.text','r+')
+        #     data3=fd.read()
+        #     fd.close()
+        #     if(new_url in data3):
+        #         print('stopppppppppp........')
+        #         break
+        # except Exception as t:
+        #     print(t)
         print('connecting...............catag '+new_url)
         file_u=cat_link[links].replace('/','_')
         print('file_u........',file_u)
-
+        if(file_u[0].isalpha()):
+            pass
+        else:
+            file_u=file_u[1:]
+        if(file_u[-1].isalpha()):
+            pass
+        else:
+            file_u=file_u[:-1]
+        new_content=''
         try:
 
             fp=open('catag/'+file_u+'.text','r')
@@ -98,176 +303,46 @@ for head in header_item[:]:
             print('file eXiSt...........')
             print()
             fp.close()
-            continue
+           
         except Exception as e:
-            try:
-                
-                if(file_u[0].isalpha()):
-                    pass
-                else:
-                    file_u=file_u[1:]
-                if(file_u[-1].isalpha()):
-                    pass
-                else:
-                    file_u=file_u[:-1]
-                print('trying..... ',new_url)
-                new_content=requests.get(new_url)
-                new_content=new_content.text
-                fp=open('catag/'+file_u+'.text','w+')
-                fp.write(new_content)
-                fp.close()
-            except Exception as e1:
-                print(e1)
+            print('trying..... ',new_url)
+            while True:
+                try:
+                    new_content=requests.get(new_url)
+                    new_content=new_content.text
+                    fp=open('catag/'+file_u+'.text','w+')
+                    fp.write(new_content)
+                    fp.close()
+                    break
+                except Exception as r:
+                    print(r)
+            
 
                 
                 
                     
 
         page_soup=Bs(new_content,'html.parser')
-
-        num_of_row=page_soup.find('li',class_='pager-current').text
-        num_of_row=int(num_of_row.split(' ')[-1])
-        
-        
-        print()
+        try:
+            num_of_row=page_soup.find('li',class_='pager-current').text
+            num_of_row=int(num_of_row.split(' ')[-1])
+        except Exception as a:
+            print(a)
+            
         print('FOR LOOP START.........for ',file_u)
-        print('linkable ..............'+file_u)
         
         for i in range(num_of_row):
-            new_url1=new_url+'?page='+str(i)
-            
-            print('connecting...............for page '+str(i))
-            
-            lunk=file_u+'_page'+str(i)+'.text'
             try:
-                fp=open('cat_page/'+lunk,'r')
-                
-                page_data=fp.read()
-                fp.close()
-                continue
-            except Exception as e:
-                try:
-                    print(new_url1)
-                    page_data=requests.get(new_url1)
-                    if(page_data.status_code==200):
-                        print('page open')
-                        print()
-                        print()
-                        fp=open('cat_page/'+lunk,'w+')
-                        fp.write(page_data.text)
-                        page_data=page_data.text
-                        print('page written')
-                        print()
-                        print()
+                print('new thread start')
+                ar=fetchdata(num_of_row,new_url,file_u,i)
+                ar.start()
+                if(ar.join()):
+                    continue
+            except Exception as e2:
+                print('thread error ',e2)
 
-                except Exception as e1:
-                    print('page error'+str(e1))
-            
-            page_1_soup=Bs(page_data,'html.parser')
-            
 
-            page_all_lis=page_1_soup.find_all('li',class_='provider-row')
-
-            
-            #all seperate 
-            count=0
-            for lis in page_all_lis:
-                li_data=lis.find('div',class_='row')
-                try:
-                    company_location_region=li_data.find('span',class_='region').text
-                except Exception as e:
-                    print('company_region not fount ')
-                    company_location_region=''
-                #print('region= '+company_location_region)
-                
-                try:
-                    company_id=li_data['data-clutch-nid']
-                except Exception as e:
-                    print('company_id not fount ')
-                    company_id=''
-                #print ('company_id='+company_id)
-                try:
-                    company_website=li_data.find('li',class_='website-link website-link-a').a['href']
-                except Exception as e:
-                    print('company_website not fount for  '+company_name)
-                    company_website=''
-
-                try:
-                    company_name=li_data.find('h3',class_='company-name').text.strip()
-                except Exception as e:
-                    ind=re.match(r'http://(www)?(.*)\.',company_website).group()
-                    company_name=ind.replace(r'http://','').replace('www.','').replace('.','')
-                if(company_name==''):
-                    ind=re.match(r'http://(www)?(.*)\.',company_website).group()
-                    company_name=ind.replace(r'http://','').replace('www.','').replace('.','')
-                    
-                
-                #print('company_name = '+company_name)
-
-                try:
-                    company_rating=li_data.find('span',class_='rating').text
-                    
-                except Exception as e:
-                    print('company_rating not fount ')
-                    company_rating=''
-                
-                #print('company_rating='+company_rating)
-                
-                try:
-                    company_review_count=li_data.find('span',class_='reviews-count').a.text.split(' ')[0]
-                
-                except Exception as e:
-                    print('company_review not fount ')
-                    company_review_count=''
-                
-                #print('company_review='+company_review_count)
-                
-                
-                try:
-                    company_employee=li_data.find('span',class_='employees').text
-                except Exception as e:
-                    print('company_emp not fount ')
-                    company_employee=''
-                
-                #print('num_employee= '+company_employee)
-
-                
-                
-                try:
-                    company_location_city=li_data.find('span',class_='locality').text[:-1]
-                except Exception as e:
-                    print('company_city not fount ')
-                    company_location_city=''
-                
-                #print('company_location_city= '+company_location_city)
-                
-                
-                
-                
-                
-                
-                
-                
-                #print('company_website= '+company_website)
-                
-                
-                
-                try:
-                    company_contact=li_data.find('div',class_='item __color6a').text.strip()
-                except Exception as e:
-                    print('company_contact not fount ')
-                    company_contact=''
-                
-                #print('company_contact='+company_contact)
-                query='insert into clutch_db values("'+company_id+'","'+file_u+'","'+company_name+'","'+company_contact+'","'+company_website+'","'+company_location_city+'","'+company_location_region+'","'+company_employee+'","'+company_rating+'","'+company_review_count+'")'
-                print('inserted company'+ company_name)
-                try:    
-                    cur.execute(query)
-                    conn.commit()
-                except Exception as e:
-                    print(e)
-                    
-
+    
                    
         
 print()
--- webscrap.py
+++ webscrap.py
@@ -2,12 +2,15 @@
 import requests
 import pymysql as sql
 import re
+import threading
+import time
+import os
 from bs4 import BeautifulSoup as Bs
 url = 'https://www.goodfirms.co'
 print(url)
 conn=sql.Connect('localhost','root','')
 cur=conn.cursor()
-
+cur.execute('use goodfirms_db')
 data = requests.get(url)
 soup = Bs(data.text,'html.parser')
 divs = soup.find_all('div',class_='frontbannerdivb')
@@ -16,6 +19,87 @@ mytarget = ['Mobile App Development','Software Development','Web Development','B
 
 subcategores={'Mobile App Development':['iphone','Android','ipad','Hybrid'],'Software Development':['PHP','Ajax','Node.js','Angularjs','Python'],'Web Development':['Wordpress'],'Blockchain Technology':['Smart Contract','Private Blockchain'],'E-commerce Development':['Shopify','Woocommerce'],'Digital Marketing':['SEO','Content Marketing','SMM','Advertising & PPC','App Marketing','Email Marketing','Local Marketing','Branding']}
 subcategores1={}
+
+class query(threading.Thread):
+    def __init__(self,name,website,location,employee,founded_in):
+        self.name=name
+        self.website=website
+        self.location=location
+        self.employee=employee
+        self.founded_in=founded_in
+    def run(self):
+        qr='insert into goodfirms_db values("'+self.name+'","'+self.website+'","'+self.location+'","'+
+        self.employee=employee
+        self.founded_in=founded_in)'
+        cur.execute(')
+
+class pagedata(threading.Thread):
+    def __init__(self,site_url,number_of_pages,dir,subdir):
+        super(pagedata,self).__init__()
+        self.site_url=site_url
+        self.number_of_pages=int(number_of_pages)
+        self.dir=dir
+        self.subdir=subdir
+    def run(self):
+        soupdata=''
+        print('loop start')
+        for i in range(self.number_of_pages):
+                try:
+                    fp=open(self.dir+'/'+self.subdir+'/page:'+str(i)+'.text','r')
+                    soupdata=fp.read()
+                    fp.close()
+                except Exception as o:
+                    print(o)
+                    while True:
+                        try:
+                            print('connecting to ',self.site_url+'/page:'+str(i))
+                            html_data=requests.get(self.site_url+'/page:'+str(i),timeout=10)
+                            soupdata=html_data.text
+                            fp=open(self.dir+'/'+self.subdir+'/page:'+str(i)+'.text','w+')
+                            fp.write(html_data.text)
+                            fp.close()
+                            break
+                        except Exception as t:
+                            print(t)
+                            pass
+                page_soup1=Bs(soupdata,'html.parser')
+                divs=page_soup1.find_all('div',class_='whitebg border radious4 margin-bottom col-md-12 no-padding greenborder overflow commoncompanydetail')
+                for div in divs:
+                    try:
+                        company_name=div.find('div',class_='company-info-title').h3.a.text
+                    except Exception as e:
+                        print(e)
+                    print('name : '+company_name)
+
+                    try:
+                        company_website=div.find('a',class_='visit-website block default-blue-btn c_visit_website')['href']
+                    except Exception as e:
+                        print(e)
+                    print('website : ' +company_website)
+
+                    spans=div.find('div',class_='clear overflow border-top border-bottom compny-service-review').find_all('div',recursive=False)
+                    print(len(spans))
+                    
+                    
+                    try:
+                        company_location=spans[1].find('div',class_='blackc font14 lhnormal').text
+                    except Exception as e:
+                        print(e)
+                    print('Location : '+company_location)
+
+                    try:
+                        company_employee=spans[2].find('div',class_='blackc font14 lhnormal').text
+                    except Exception as e:
+                        print(e)
+                    print('Employee : '+company_employee)  
+
+                    try:
+                        company_Founded_in=spans[3].find('div',class_='blackc font14 lhnormal').text
+                    except Exception as e:
+                        print(e)
+                    print('found : '+company_Founded_in)
+ 
+                    a=input('hello:')        
 
 for data in divs:
     if data.a.text in mytarget:
@@ -42,19 +126,18 @@ print(len(subcategores1))
 fetchdata=target[mytarget[3]]
 database_name=mytarget[3].split(' ')
 database_name='_'.join(database_name)
-try:
-    database=cur.execute('create database '+database_name)
-except Exception as e:
-    print(e)
+os.system('mkdir '+database_name)
 
-cur.execute('use '+database_name)
+
 tablename=subcategores[mytarget[3]][0]
 tablename=tablename.split(' ')
 tablename='_'.join(tablename)
 print('table name :'+tablename)
+
+os.system('mkdir '+database_name+'/'+tablename)
 try:
-    var='create table '+tablename+'(Company_Name varchar(100),Company_Website varchar(100),Company_Location varchar(100),Company_Employee varchar(100),Company_Founded_in varchar(100))'
-    print(var)
+    var='create table goodfirms_data(Company_Name varchar(100),Company_Website varchar(100),Company_Location varchar(100),Company_Employee varchar(100),Company_Founded_in varchar(100))'
+    
     cur.execute(var)
 except Exception as e:
     print(e)
@@ -63,24 +146,23 @@ fetchdata=url+subcategores1[subcategores[mytarget[4]][0]]
 
 print('Link is :-'+fetchdata)
 page=requests.get(fetchdata)
-
+print(fetchdata)
 pagesoup=Bs(page.text,'html.parser')
 #print(pagesoup.prettify())
 flag=0
 try:
     number_of_page=pagesoup.find_all('a',href=True,title='Last page')[0]['href']
     indexes= number_of_page.index('page')
-    print(indexes)
     number_of_page=number_of_page[indexes+5:]
     print(number_of_page+' pages found in this Catagory')
     flag=1
 except Exception as e:
     print(e)
-if(!flag)
+if(flag!=0):
+    #new loop for all pages
     
-#new loop for all pages
-fetchdata+='/page:'+str(1)
-company_data=requests.get(fetchdata)
-company_pagesoup=Bs(company_data.text,'html.parser')
+    pagedata(fetchdata,number_of_page,database_name,tablename).start()
+    
+    
 
 
